e42cde3ef804e89e77dabdac4156bc8dd10b367b
jupiter <pengjupiter5@gmail.com>
2024-01-28 10:55:00 -0600

    formatting

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -46,28 +46,28 @@ void finishMoveBoard(GameState* state) {
   }
 }
 
-  int getBulletValueAt(GameState* state, size_t x, size_t y) {
-    int ret = 0;
-    for (size_t i = 0; i < state->board[x][y].size(); ++i) {
-      ret += state->board[x][y][i]->getBulletValue(state, x, y, i);
-    }
-    return ret;
+int getBulletValueAt(GameState* state, size_t x, size_t y) {
+  int ret = 0;
+  for (size_t i = 0; i < state->board[x][y].size(); ++i) {
+    ret += state->board[x][y][i]->getBulletValue(state, x, y, i);
   }
+  return ret;
+}
 
-  bool canMove(GameState* state, size_t x, size_t y, int vx, int vy,
-               Tile* curTile) {
-    size_t x2 = x + vx;
-    size_t y2 = y + vy;
-    // unsigned, so never negative
-    if (x2 >= state->board.size() || y2 >= state->board[x].size() ||
-        y2 >= state->board[x2].size()) {
+bool canMove(GameState* state, size_t x, size_t y, int vx, int vy,
+             Tile* curTile) {
+  size_t x2 = x + vx;
+  size_t y2 = y + vy;
+  // unsigned, so never negative
+  if (x2 >= state->board.size() || y2 >= state->board[x].size() ||
+      y2 >= state->board[x2].size()) {
+    return false;
+  }
+  for (size_t i = 0; i < state->board[x2][y2].size(); ++i) {
+    if (state->board[x2][y2][i]->isSolidFor(state, x2, y2, i, curTile)) {
       return false;
     }
-    for (size_t i = 0; i < state->board[x2][y2].size(); ++i) {
-      if (state->board[x2][y2][i]->isSolidFor(state, x2, y2, i, curTile)) {
-        return false;
-      }
-    }
-
-    return true;
   }
+
+  return true;
+}
